84fbec6f0ba5b4f324b738cf60684ee9ff376967
Wing-Hou Chan <wing@wii.ng>
2024-08-02 15:05:44 +0100

    build(lint): configure linting rule for import order

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,23 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: ['expo', 'prettier'],
+  extends: ['expo', 'plugin:import/recommended', 'prettier'],
   plugins: ['prettier'],
   rules: {
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        groups: [
+          ['builtin', 'external', 'internal'],
+          'parent',
+          'sibling',
+          'index',
+        ],
+      },
+    ],
     'prettier/prettier': 'error',
   },
 }
