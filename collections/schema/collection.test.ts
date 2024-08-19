f3da79871dc7c374a8d40db6d715d27d766c9cdd
Wing-Hou Chan <wing@wii.ng>
2024-08-19 17:11:51 +0100

    test(collection): add improvements to collection schema tests
    Includes refactoring duplicate code and adjusting precision of expectations.

--- collections/schema/collection.test.ts
+++ collections/schema/collection.test.ts
@@ -1,199 +1,158 @@
 import { mockDatabase } from '@/test/utils'
 import { collection } from './collection'
 
+type DatabaseMock = Awaited<ReturnType<typeof mockDatabase>>
+type Any<Type> = {
+  [Property in keyof Type]: any
+}
+
+const collectionMock = {
+  name: 'Collection 1',
+}
+
 describe('`collection` table', () => {
+  let database: DatabaseMock['database'],
+    resetDatabaseMock: DatabaseMock['resetDatabaseMock'],
+    insertCollection: (
+      values: Any<typeof collection.$inferInsert>,
+    ) => Promise<typeof collection.$inferSelect>
+
+  beforeEach(async () => {
+    ;({ database, resetDatabaseMock } = await mockDatabase())
+
+    insertCollection = async (values) =>
+      (await database.insert(collection).values(values).returning())[0]
+  })
+
+  afterEach(() => {
+    resetDatabaseMock()
+  })
+
   describe('`id` column', () => {
     it('is an integer', async () => {
-      const { database, resetDatabaseMock } = await mockDatabase()
-      const insertCollection = async ({ id }: { id: any }) =>
-        (
-          await database
-            .insert(collection)
-            .values({ id, name: 'Collection 1' })
-            .returning({ id: collection.id })
-        )[0]
+      const datatypeMismatch = expect.objectContaining({
+        message: expect.stringContaining('datatype mismatch'),
+      })
 
-      await expect(insertCollection({ id: 'string' })).rejects.toEqual(
-        expect.objectContaining({
-          message: expect.stringContaining('datatype mismatch'),
-        }),
-      )
-      await expect(insertCollection({ id: 0.1 })).rejects.toEqual(
-        expect.objectContaining({
-          message: expect.stringContaining('datatype mismatch'),
-        }),
-      )
-      await expect(insertCollection({ id: 1 })).resolves.toEqual({ id: 1 })
-
-      resetDatabaseMock()
+      await expect(
+        insertCollection({ ...collectionMock, id: 'string' }),
+      ).rejects.toEqual(datatypeMismatch)
+      await expect(
+        insertCollection({ ...collectionMock, id: 0.1 }),
+      ).rejects.toEqual(datatypeMismatch)
+      await expect(
+        insertCollection({ ...collectionMock, id: 1 }),
+      ).resolves.toEqual(expect.objectContaining({ id: expect.any(Number) }))
     })
 
     it('cannot be `null`', async () => {
-      const { database, resetDatabaseMock } = await mockDatabase()
-      const insertCollectionWithNullId = async () =>
-        (
-          await database
-            .insert(collection)
-            .values({
-              // Cast `null` to `number` so that compile time checks pass and run time checks can be tested
-              id: null as unknown as number,
-              name: 'Collection 1',
-            })
-            .returning({ id: collection.id })
-        )[0]
-
-      const output = await insertCollectionWithNullId()
-
-      expect(output).toEqual({ id: 1 })
-
-      resetDatabaseMock()
+      await expect(
+        insertCollection({ ...collectionMock, id: undefined }),
+      ).resolves.toEqual(expect.objectContaining({ id: expect.any(Number) }))
+      await expect(
+        insertCollection({ ...collectionMock, id: null }),
+      ).resolves.toEqual(expect.objectContaining({ id: expect.any(Number) }))
     })
 
     it('auto-increments', async () => {
-      const { database, resetDatabaseMock } = await mockDatabase()
-      const insertCollection = async (name: string) =>
-        (
-          await database
-            .insert(collection)
-            .values({ name })
-            .returning({ id: collection.id })
-        )[0]
-
       const output = [
-        await insertCollection('Collection 1'),
-        await insertCollection('Collection 2'),
+        await insertCollection({ name: 'Collection 1' }),
+        await insertCollection({ name: 'Collection 2' }),
       ]
 
-      expect(output).toEqual([{ id: 1 }, { id: 2 }])
-
-      resetDatabaseMock()
+      expect(output).toEqual([
+        expect.objectContaining({ id: 1 }),
+        expect.objectContaining({ id: 2 }),
+      ])
     })
   })
 
   describe('`name` column', () => {
     it('is a string', async () => {
-      const { database, resetDatabaseMock } = await mockDatabase()
-      const insertCollection = async ({ name }: { name: any }) =>
-        (
-          await database
-            .insert(collection)
-            .values({ name })
-            .returning({ name: collection.name })
-        )[0]
-
-      await expect(insertCollection({ name: 0.1 })).resolves.toEqual({
-        name: '0.1',
-      })
-      await expect(insertCollection({ name: 1 })).resolves.toEqual({
-        name: '1.0',
-      })
-      await expect(insertCollection({ name: 'string' })).resolves.toEqual({
-        name: 'string',
-      })
-
-      resetDatabaseMock()
+      await expect(insertCollection({ name: 0.1 })).resolves.toEqual(
+        expect.objectContaining({
+          name: '0.1',
+        }),
+      )
+      await expect(insertCollection({ name: 1 })).resolves.toEqual(
+        expect.objectContaining({
+          name: '1.0',
+        }),
+      )
+      await expect(insertCollection({ name: 'string' })).resolves.toEqual(
+        expect.objectContaining({
+          name: 'string',
+        }),
+      )
     })
 
     it('cannot be `null`', async () => {
-      const { database, resetDatabaseMock } = await mockDatabase()
-      const insertCollectionWithNullName = async () =>
-        await database.insert(collection).values({
-          // Cast `null` to `string` so that compile time checks pass and run time checks can be tested
-          name: null as unknown as string,
-        })
+      const notNullConstraintFailed = expect.objectContaining({
+        message: expect.stringContaining(
+          'NOT NULL constraint failed: collection.name',
+        ),
+      })
 
-      await expect(insertCollectionWithNullName).rejects.toEqual(
-        expect.objectContaining({
-          message: expect.stringContaining(
-            'NOT NULL constraint failed: collection.name',
-          ),
-        }),
+      await expect(insertCollection({ name: undefined })).rejects.toEqual(
+        notNullConstraintFailed,
       )
-
-      resetDatabaseMock()
+      await expect(insertCollection({ name: null })).rejects.toEqual(
+        notNullConstraintFailed,
+      )
     })
 
     it('cannot be an empty string', async () => {
-      const { database, resetDatabaseMock } = await mockDatabase()
-      const insertCollectionWithEmptyName = async () =>
-        await database.insert(collection).values({ name: '' })
-
-      await expect(insertCollectionWithEmptyName).rejects.toEqual(
+      await expect(insertCollection({ name: '' })).rejects.toEqual(
         expect.objectContaining({
           message: expect.stringContaining('CHECK constraint failed: name'),
         }),
       )
-
-      resetDatabaseMock()
     })
   })
 
   describe('`created_at` column', () => {
     it('is a date', async () => {
-      const { database, resetDatabaseMock } = await mockDatabase()
-      const insertCollection = async ({ created_at }: { created_at: any }) =>
-        (
-          await database
-            .insert(collection)
-            .values({ created_at, name: 'Collection 1' })
-            .returning({ createdAt: collection.created_at })
-        )[0]
       const now = new Date()
 
       await expect(() =>
-        insertCollection({ created_at: 'string' }),
+        insertCollection({ ...collectionMock, created_at: 'string' }),
       ).rejects.toThrow()
       await expect(() =>
-        insertCollection({ created_at: 0.1 }),
+        insertCollection({ ...collectionMock, created_at: 0.1 }),
       ).rejects.toThrow()
-      await expect(() => insertCollection({ created_at: 1 })).rejects.toThrow()
-      await expect(insertCollection({ created_at: now })).resolves.toEqual({
-        createdAt: now,
-      })
-
-      resetDatabaseMock()
+      await expect(() =>
+        insertCollection({ ...collectionMock, created_at: 1 }),
+      ).rejects.toThrow()
+      await expect(
+        insertCollection({ ...collectionMock, created_at: now }),
+      ).resolves.toEqual(
+        expect.objectContaining({
+          created_at: now,
+        }),
+      )
     })
 
     it('defaults to _now_', async () => {
-      const { database, resetDatabaseMock } = await mockDatabase()
-      const insertCollection = async () =>
-        (
-          await database
-            .insert(collection)
-            .values({ name: 'Collection 1' })
-            .returning({ createdAt: collection.created_at })
-        )[0]
-
-      const { createdAt } = await insertCollection()
+      const { created_at } = await insertCollection(collectionMock)
 
       // The `created_at` datetime is determined in the database and not something that can be mocked.
       // Expect it to be within 1000 ms of when the assertion is executed.
-      expect(createdAt).toBeBetween(
+      expect(created_at).toBeBetween(
         new Date(new Date().valueOf() - 1000),
         new Date(),
       )
-
-      resetDatabaseMock()
     })
 
     it('cannot be `null`', async () => {
-      const { database, resetDatabaseMock } = await mockDatabase()
-      const insertCollectionWithNullCreatedAt = async () =>
-        await database.insert(collection).values({
-          // Cast `null` to `Date` so that compile time checks pass and run time checks can be tested
-          created_at: null as unknown as Date,
-          name: 'Collection 1',
-        })
-
-      await expect(insertCollectionWithNullCreatedAt).rejects.toEqual(
+      await expect(
+        insertCollection({ ...collectionMock, created_at: null }),
+      ).rejects.toEqual(
         expect.objectContaining({
           message: expect.stringContaining(
             'NOT NULL constraint failed: collection.created_at',
           ),
         }),
       )
-
-      resetDatabaseMock()
     })
   })
 })
