b7a87b508027f8aa115e7c23496bb99ff730d8f5
Wing-Hou Chan <wing@wii.ng>
2024-10-15 13:59:02 +0100

    build(test): enable auto-clearing mocks before tests
    Improves the independence of tests. See https://jestjs.io/docs/configuration#clearmocks-boolean.

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ import type { JestConfigWithTsJest } from 'ts-jest'
 
 const config: JestConfigWithTsJest = {
   preset: 'jest-expo',
+  clearMocks: true,
   moduleDirectories: ['node_modules', '<rootDir>'],
   setupFilesAfterEnv: ['./test/setup.ts'],
   testPathIgnorePatterns: ['e2e'],
