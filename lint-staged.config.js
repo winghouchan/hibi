02af53576c8ecce9adf34c6aab270229f2d71e6f
Wing-Hou Chan <wing@wii.ng>
2024-11-08 13:51:56 +0000

    build(commit): extract strings for localization on commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,16 @@
 module.exports = {
   '**/!(*.{js,jsx,ts,tsx})': ['prettier --write --ignore-unknown'],
-  '*.{js,jsx}': ['eslint --fix', 'prettier --write --ignore-unknown'],
+  '*.{js,jsx}': [
+    'eslint --fix',
+    'prettier --write --ignore-unknown',
+    'yarn run intl:extract --clean',
+    'git add ./src/intl/messages',
+  ],
   '*.{ts,tsx}': () => [
     'tsc --project tsconfig.json --noEmit',
     'eslint --fix',
     'prettier --write --ignore-unknown',
+    'yarn run intl:extract --clean',
+    'git add ./src/intl/messages',
   ],
 }
