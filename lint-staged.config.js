92fb8faa8a0429bfa9b9a7b886f4dc838e03f31b
Wing-Hou Chan <wing@wii.ng>
2024-11-08 14:04:48 +0000

    build(commit): refactor lint-staged configuration

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,16 +1,9 @@
+const typeCheck = ['tsc --project tsconfig.json --noEmit']
+const lint = ['eslint --fix', 'prettier --write --ignore-unknown']
+const intl = ['yarn run intl:extract --clean', 'git add ./src/intl/messages']
+
 module.exports = {
-  '**/!(*.{js,jsx,ts,tsx})': ['prettier --write --ignore-unknown'],
-  '*.{js,jsx}': [
-    'eslint --fix',
-    'prettier --write --ignore-unknown',
-    'yarn run intl:extract --clean',
-    'git add ./src/intl/messages',
-  ],
-  '*.{ts,tsx}': () => [
-    'tsc --project tsconfig.json --noEmit',
-    'eslint --fix',
-    'prettier --write --ignore-unknown',
-    'yarn run intl:extract --clean',
-    'git add ./src/intl/messages',
-  ],
+  '**/!(*.{js,jsx,ts,tsx})': [...lint],
+  '*.{js,jsx}': [...lint, ...intl],
+  '*.{ts,tsx}': () => [...typeCheck, ...lint, ...intl],
 }
