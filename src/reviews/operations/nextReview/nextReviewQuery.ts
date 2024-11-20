54b9a1161992fcd7b22e41554b95a4622269b4c4
Wing-Hou Chan <wing@wii.ng>
2024-11-20 16:51:25 +0000

    refactor(review): use react-query's infinite query to handle fetching due reviewables

--- src/reviews/operations/nextReview/nextReviewQuery.ts
+++ src/reviews/operations/nextReview/nextReviewQuery.ts
@@ -1,7 +1,13 @@
-import { queryOptions } from '@tanstack/react-query'
+import { infiniteQueryOptions } from '@tanstack/react-query'
 import getNextReview from './getNextReview'
 
-export default queryOptions({
-  queryKey: ['reviewable', 'next'],
-  queryFn: async () => await getNextReview({ onlyDue: true }),
-})
+export default (options: Parameters<typeof getNextReview>[0]) =>
+  infiniteQueryOptions({
+    queryKey: ['review', options],
+    queryFn: async () => await getNextReview(options),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages, lastPageParam) =>
+      lastPage === null ? undefined : lastPageParam + 1,
+    getPreviousPageParam: (firstPage, allPages, firstPageParam) =>
+      firstPageParam <= 1 ? undefined : firstPageParam - 1,
+  })
--- src/reviews/ui/ReviewScreen/index.tsx
+++ src/reviews/ui/ReviewScreen/index.tsx
@@ -1,6 +1,6 @@
 import { Trans } from '@lingui/macro'
-import { useQuery } from '@tanstack/react-query'
-import { useEffect, useRef, useState } from 'react'
+import { useInfiniteQuery } from '@tanstack/react-query'
+import { useEffect, useRef } from 'react'
 import { View } from 'react-native'
 import PagerView from 'react-native-pager-view'
 import { nextReviewQuery } from '@/reviews/operations'
@@ -8,48 +8,34 @@ import Review from './Review'
 import ReviewFinished from './ReviewFinished'
 
 export default function ReviewScreen() {
-  const { data: nextReview, refetch } = useQuery(nextReviewQuery)
-  const pagerViewRef = useRef<PagerView>(null)
   const initialPage = 0
-  const [page, setPage] = useState(initialPage)
-  const [reviewables, setReviewables] = useState<(typeof nextReview)[]>([])
+  const pagerViewRef = useRef<PagerView>(null)
+  const query = nextReviewQuery({ onlyDue: true })
+  const { data, fetchNextPage } = useInfiniteQuery(query)
 
-  const onNextReview = () => {
-    setReviewables((state) => {
-      const newState =
-        state.at(-1)?.id !== nextReview?.id ? [...state, nextReview] : state
-
-      setPage(newState.length - 1)
-
-      return newState
-    })
-  }
-
-  const onPageChange = () => {
-    pagerViewRef.current?.setPage(page)
+  const onNewPage = () => {
+    data?.pages.length && pagerViewRef.current?.setPage(data.pages.length - 1)
   }
 
   const onReview = async () => {
-    await refetch()
+    await fetchNextPage()
   }
 
-  useEffect(onNextReview, [nextReview])
-
-  useEffect(onPageChange, [page])
+  useEffect(onNewPage, [data?.pages.length])
 
   return (
     <View testID="review.screen" style={{ flex: 1 }}>
       <View>
         <Trans>Review Screen</Trans>
       </View>
-      {reviewables.length > 0 && (
+      {(data?.pages.length ?? 0) > 0 && (
         <PagerView
           initialPage={initialPage}
           ref={pagerViewRef}
           scrollEnabled={false}
           style={{ flex: 1 }}
         >
-          {reviewables.map((reviewable, index) =>
+          {data?.pages.map((reviewable, index) =>
             reviewable ? (
               <Review key={index} {...reviewable} onReview={onReview} />
             ) : (
