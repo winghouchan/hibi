5aa700ff044fa4966c3c7c8d6e971f3b05bfb7bc
Wing-Hou Chan <wing@wii.ng>
2024-12-13 12:21:11 +0000

    fix(intl): fix incorrect message origin paths in translation catalogs
    The issue was caused by Lingui only writing message origin paths from files that were specified
    in the extract command, overwriting existing paths. See the following GitHub issue:
    https://github.com/lingui/js-lingui/issues/2122.
    
    Resolving by removing message extraction on pre-commit, which removes the need for running the
    extract command with the paths of changed files. Translation catalog files (`.po`) will be managed
    by a tool like a translation management system and be derived from the template file (`.pot).
    
    For local development, messages will fallback to the source messages. For pseudo-localization, it
    will continue to be generated during `intl:compile`.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,19 +3,10 @@ const lint = (files) =>
   ['eslint --fix', 'prettier --write --ignore-unknown'].map(
     (command) => `${command} ${files.join(' ')}`,
   )
-const intl = (files) => [
-  `yarn run intl:extract --clean ${files.join(' ')}`,
-  'git add ./src/intl/messages',
-]
 const test = ['jest --onlyChanged']
 
 module.exports = {
   '**/!(*.{js,jsx,ts,tsx})': (files) => [...lint(files)],
-  '*.{js,jsx}': (files) => [...lint(files), ...test, ...intl(files)],
-  '*.{ts,tsx}': (files) => [
-    ...typeCheck,
-    ...lint(files),
-    ...test,
-    ...intl(files),
-  ],
+  '*.{js,jsx}': (files) => [...lint(files), ...test],
+  '*.{ts,tsx}': (files) => [...typeCheck, ...lint(files), ...test],
 }
