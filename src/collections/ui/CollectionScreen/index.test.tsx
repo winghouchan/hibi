5dc790f6ff1d8deb64aee769e64fd3f34332e150
Wing-Hou Chan <wing@wii.ng>
2025-02-15 14:29:54 +0000

    test(collection): refactor and add test for error while fetching collection in collection screen

--- src/collections/ui/CollectionScreen/index.test.tsx
+++ src/collections/ui/CollectionScreen/index.test.tsx
@@ -1,58 +1,82 @@
 import { screen, waitFor } from '@testing-library/react-native'
 import { renderRouter } from 'expo-router/testing-library'
 import { Alert } from 'react-native'
-import { mockCollection } from '@/collections/test'
+import { mockCollection, mockCollectionError } from '@/collections/test'
 import { mockAppRoot } from 'test/utils'
 import CollectionScreen from '.'
 
 describe('<CollectionScreen />', () => {
-  test('when there is a collection ID and the collection exists, it is displayed', async () => {
-    const fixture = {
-      collection: {
-        id: 1,
-        name: 'Collection Name',
-        createdAt: new Date(),
-        notes: [],
-      },
-    }
+  describe('when there is a collection ID', () => {
+    test('and the collection exists, the collection is displayed', async () => {
+      const fixture = {
+        collection: {
+          id: 1,
+          name: 'Collection Name',
+          createdAt: new Date(),
+          notes: [],
+        },
+      }
 
-    mockCollection(fixture.collection)
+      mockCollection(fixture.collection)
 
-    renderRouter(
-      {
-        'collection/[id]': CollectionScreen,
-      },
-      {
-        initialUrl: '/collection/1',
-        wrapper: mockAppRoot(),
-      },
-    )
+      renderRouter(
+        {
+          'collection/[id]': CollectionScreen,
+        },
+        {
+          initialUrl: `/collection/${fixture.collection.id}`,
+          wrapper: mockAppRoot(),
+        },
+      )
 
-    await waitFor(async () => {
-      expect(await screen.findByText(fixture.collection.name)).toBeOnTheScreen()
+      await waitFor(async () => {
+        expect(
+          await screen.findByText(fixture.collection.name),
+        ).toBeOnTheScreen()
+      })
     })
-  })
 
-  test('when there is a collection ID and the collection does not exist, the user is alerted', async () => {
-    const alertSpy = jest.spyOn(Alert, 'alert')
-    const fixture = {
-      collection: null,
-    }
+    test('and the collection does not exist, an alert is displayed', async () => {
+      const alertSpy = jest.spyOn(Alert, 'alert')
+      const fixture = {
+        collection: null,
+      }
 
-    mockCollection(fixture.collection)
+      mockCollection(fixture.collection)
 
-    renderRouter(
-      {
-        'collection/[id]': CollectionScreen,
-      },
-      {
-        initialUrl: '/collection/0',
-        wrapper: mockAppRoot(),
-      },
-    )
+      renderRouter(
+        {
+          'collection/[id]': CollectionScreen,
+        },
+        {
+          initialUrl: '/collection/0',
+          wrapper: mockAppRoot(),
+        },
+      )
 
-    await waitFor(async () => {
-      expect(alertSpy).toHaveBeenCalledOnce()
+      await waitFor(() => {
+        expect(alertSpy).toHaveBeenCalledOnce()
+      })
+    })
+
+    test('and there was an error fetching the collection, an alert is displayed', async () => {
+      const alertSpy = jest.spyOn(Alert, 'alert')
+
+      mockCollectionError(new Error('Mock Error'))
+
+      renderRouter(
+        {
+          'collection/[id]': CollectionScreen,
+        },
+        {
+          initialUrl: '/collection/1',
+          wrapper: mockAppRoot(),
+        },
+      )
+
+      await waitFor(() => {
+        expect(alertSpy).toHaveBeenCalledOnce()
+      })
     })
   })
 })
