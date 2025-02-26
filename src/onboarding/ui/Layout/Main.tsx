609eed90961b3966feff6ede307d4de351ae3e68
Wing-Hou Chan <wing@wii.ng>
2025-02-26 20:38:21 +0000

    refactor(onboarding): split layout components into individual files with their own styles
    Better workaround to the issue of padding not updating when the keyboard inset changes.

---
+++ src/onboarding/ui/Layout/Main.tsx
@@ -0,0 +1,30 @@
+import { ScrollView, ScrollViewProps } from 'react-native'
+import { StyleSheet } from 'react-native-unistyles'
+
+const styles = StyleSheet.create((theme, { insets }) => ({
+  view: {
+    flex: 1,
+  },
+
+  contentContainer: {
+    paddingTop: theme.spacing[2],
+    paddingLeft: insets.left + theme.spacing[4],
+    paddingRight: insets.right + theme.spacing[4],
+    paddingBottom: theme.spacing[2],
+  },
+}))
+
+export default function Main({
+  contentContainerStyle,
+  style,
+  ...props
+}: ScrollViewProps) {
+  return (
+    <ScrollView
+      alwaysBounceVertical={false}
+      contentContainerStyle={[styles.contentContainer, contentContainerStyle]}
+      style={[styles.view, style]}
+      {...props}
+    />
+  )
+}
--- src/onboarding/ui/Layout/index.tsx
+++ src/onboarding/ui/Layout/index.tsx
@@ -1,43 +1,13 @@
-import { ScrollView, ScrollViewProps, View, ViewProps } from 'react-native'
-import { StyleSheet } from 'react-native-unistyles'
-import style from './style'
+import Container from './Container'
+import Footer from './Footer'
+import Main from './Main'
 
-/**
- * This style sheet needs to be defined here otherwise the bottom padding will
- * not dynamically update when the keyboard opens/closes. Root cause yet to be
- * determined. The `insets` also cannot be destructured from `runtime`.
- *
- * @see {@link} https://github.com/jpudysz/react-native-unistyles/issues/623
- */
-const { screen } = StyleSheet.create((theme, runtime) => ({
-  screen: {
-    flex: 1,
-    paddingBottom: runtime.insets.ime,
-  },
-}))
-
-function Layout({ style: _style, ...props }: ViewProps) {
-  return <View style={[screen, _style]} {...props} />
+type LayoutComponents = typeof Container & {
+  Main: typeof Main
+  Footer: typeof Footer
 }
 
-function Main({
-  contentContainerStyle: _contentContainerStyle,
-  style: _style,
-  ...props
-}: ScrollViewProps) {
-  return (
-    <ScrollView
-      alwaysBounceVertical={false}
-      contentContainerStyle={[style.content, _contentContainerStyle]}
-      style={[style.main, _style]}
-      {...props}
-    />
-  )
-}
-
-function Footer({ style: _style, ...props }: ViewProps) {
-  return <View style={[style.footer, _style]} {...props} />
-}
+const Layout = Container as LayoutComponents
 
 Layout.Main = Main
 Layout.Footer = Footer
