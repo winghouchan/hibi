9ff8d0bf2e02df1276f77aa3e17d80e03db9eea9
Wing-Hou Chan <wing@wii.ng>
2025-02-28 17:13:46 +0000

    feat(ui): add basic styling for text input

--- src/ui/TextInput/index.test.tsx
+++ src/ui/TextInput/index.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react-native'
+import { render, screen, userEvent } from '@testing-library/react-native'
 import TextInput from '.'
 
 describe('<TextInput />', () => {
@@ -6,5 +6,30 @@ describe('<TextInput />', () => {
     render(<TextInput accessibilityLabel="Test input field" testID="test" />)
 
     expect(screen.getByTestId('test.input')).toBeOnTheScreen()
+  })
+
+  test('calls event handlers', async () => {
+    const user = userEvent.setup()
+    const input = 'Input'
+
+    const handlers = {
+      onBlur: jest.fn(),
+      onChange: jest.fn(),
+      onChangeText: jest.fn(),
+      onFocus: jest.fn(),
+      onSubmitEditing: jest.fn(),
+    }
+
+    render(<TextInput accessibilityLabel="Input" {...handlers} />)
+
+    await user.type(screen.getByLabelText('Input'), input, {
+      submitEditing: true,
+    })
+
+    expect(handlers.onBlur).toHaveBeenCalledOnce()
+    expect(handlers.onChange).toHaveBeenCalledTimes(input.length)
+    expect(handlers.onChangeText).toHaveBeenCalledTimes(input.length)
+    expect(handlers.onFocus).toHaveBeenCalledOnce()
+    expect(handlers.onSubmitEditing).toHaveBeenCalledOnce()
   })
 })
--- src/ui/TextInput/index.tsx
+++ src/ui/TextInput/index.tsx
@@ -1,7 +1,64 @@
+import { useState } from 'react'
 import ReactNative, { TextInputProps } from 'react-native'
+import { StyleSheet } from 'react-native-unistyles'
 
-export default function TextInput({ testID, ...props }: TextInputProps) {
+const styles = StyleSheet.create(
+  ({ borderWidths, colors, fontSizes, radii, spacing }) => ({
+    input: {
+      borderColor: colors.neutral[4],
+      borderRadius: radii[4],
+      borderWidth: borderWidths[2],
+      fontSize: fontSizes[2],
+      padding: spacing[4],
+      width: '100%',
+
+      variants: {
+        focused: {
+          true: {
+            borderColor: colors.neutral[6],
+          },
+        },
+      },
+    },
+  }),
+)
+
+export default function TextInput({
+  onBlur,
+  onFocus,
+  style,
+  testID,
+  ...props
+}: TextInputProps) {
+  /**
+   * Allows changes to variants to change the style
+   *
+   * @see {@link https://github.com/jpudysz/react-native-unistyles/issues/368}
+   */
+  // eslint-disable-next-line react-compiler/react-compiler
+  'use no memo'
+
+  const [focused, setFocused] = useState(false)
+
+  const handleBlur: TextInputProps['onBlur'] = (...args) => {
+    setFocused(false)
+    onBlur?.(...args)
+  }
+
+  const handleFocus: TextInputProps['onFocus'] = (...args) => {
+    setFocused(true)
+    onFocus?.(...args)
+  }
+
+  styles.useVariants({ focused })
+
   return (
-    <ReactNative.TextInput testID={testID && `${testID}.input`} {...props} />
+    <ReactNative.TextInput
+      onBlur={handleBlur}
+      onFocus={handleFocus}
+      style={[styles.input, style]}
+      testID={testID && `${testID}.input`}
+      {...props}
+    />
   )
 }
