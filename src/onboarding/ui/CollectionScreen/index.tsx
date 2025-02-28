ef653214f7d2e73e0027c5e3a4e7941acbbf2121
Wing-Hou Chan <wing@wii.ng>
2025-02-28 19:21:40 +0000

    refactor(onboarding): factor out form logic on onboarding collection screen

--- src/onboarding/ui/CollectionScreen/index.tsx
+++ src/onboarding/ui/CollectionScreen/index.tsx
@@ -1,136 +1,91 @@
 import { msg, Trans } from '@lingui/macro'
 import { useLingui } from '@lingui/react'
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { useQuery } from '@tanstack/react-query'
 import { router } from 'expo-router'
-import { Formik, type FormikConfig } from 'formik'
 import { Alert } from 'react-native'
 import { object, string } from 'yup'
-import {
-  createCollectionMutation,
-  updateCollectionMutation,
-} from '@/collections'
-import { log } from '@/telemetry'
 import { Button, TextField } from '@/ui'
 import { onboardingCollectionQuery } from '../../operations'
 import Layout from '../Layout'
+import useForm from './useForm'
 
 export default function CollectionScreen() {
   const { i18n } = useLingui()
-  const queryClient = useQueryClient()
   const { data: collection } = useQuery(onboardingCollectionQuery)
-  const { mutateAsync: createCollection } = useMutation(
-    createCollectionMutation,
-  )
-  const { mutateAsync: updateCollection } = useMutation(
-    updateCollectionMutation,
-  )
 
-  const initialValues = {
-    id: collection?.id,
-    name: collection?.name ?? '',
+  const onSubmitSuccess: Parameters<
+    typeof useForm
+  >[0]['onSubmitSuccess'] = () => {
+    router.push('/onboarding/notes')
+  }
+
+  const onSubmitError: Parameters<typeof useForm>[0]['onSubmitError'] = (
+    error,
+    retry,
+  ) => {
+    Alert.alert(
+      i18n.t(msg`Something went wrong`),
+      typeof collection?.id === 'undefined'
+        ? i18n.t(msg`There was an error creating the collection`)
+        : i18n.t(msg`There was an error updating the collection`),
+      [
+        {
+          text: i18n.t(msg`Try again`),
+          style: 'default',
+          isPreferred: true,
+          onPress: retry,
+        },
+        {
+          text: i18n.t(msg`Cancel`),
+          style: 'cancel',
+        },
+      ],
+    )
   }
 
   const validationSchema = object({
     name: string().required(i18n.t(msg`Your collection needs a name`)),
   })
 
-  const onSubmit: FormikConfig<typeof initialValues>['onSubmit'] = async (
-    values,
-    { setFieldValue },
-  ) => {
-    type SubmitFunction<
-      Values extends
-        | Parameters<typeof updateCollection>[0]
-        | Parameters<typeof createCollection>[0],
-    > = Values extends Parameters<typeof updateCollection>[0]
-      ? typeof updateCollection
-      : typeof createCollection
-
-    const doesCollectionExist = values.id !== undefined
-
-    const submit = (
-      doesCollectionExist ? updateCollection : createCollection
-    ) as SubmitFunction<typeof values>
-
-    const handlers: Parameters<typeof submit>[1] = {
-      async onSuccess({ id }) {
-        await queryClient.invalidateQueries({
-          queryKey: onboardingCollectionQuery.queryKey,
-        })
-        await setFieldValue('id', id)
-        router.push('/onboarding/notes')
-      },
-      onError(error) {
-        // @todo Handle error
-        Alert.alert(
-          i18n.t(msg`Something went wrong`),
-          doesCollectionExist
-            ? i18n.t(msg`There was an error updating the collection`)
-            : i18n.t(msg`There was an error creating the collection`),
-          [
-            {
-              text: i18n.t(msg`Try again`),
-              style: 'default',
-              isPreferred: true,
-              onPress: async () => {
-                await submit(values, handlers)
-              },
-            },
-            {
-              text: i18n.t(msg`Cancel`),
-              style: 'cancel',
-            },
-          ],
-        )
-        log.error(error)
-      },
-    }
-
-    try {
-      await submit(values, handlers)
-    } catch {
-      // Errors handled by `submit`'s error handler
-    }
-  }
+  const { errors, handleChange, handleSubmit, isSubmitting, values } = useForm({
+    initialValues: {
+      id: collection?.id,
+      name: collection?.name ?? '',
+    },
+    onSubmitSuccess,
+    onSubmitError,
+    validationSchema,
+  })
 
   return (
-    <Formik
-      enableReinitialize
-      initialValues={initialValues}
-      onSubmit={onSubmit}
-      validationSchema={validationSchema}
-    >
-      {({ errors, handleChange, handleSubmit, isSubmitting, values }) => (
-        <Layout testID="onboarding.collection.screen">
-          <Layout.Main>
-            <Trans>What are you learning?</Trans>
-            <TextField
-              accessibilityLabel={i18n.t(msg`Enter a collection name`)}
-              autoFocus
-              error={errors.name}
-              onChangeText={(value) => handleChange('name')(value)}
-              onSubmitEditing={() => handleSubmit()}
-              placeholder={i18n.t(msg`Collection name`)}
-              testID="onboarding.collection.name"
-              value={values.name}
-            />
-          </Layout.Main>
-          <Layout.Footer>
-            <Button
-              testID="onboarding.collection.cta"
-              onPress={() => handleSubmit()}
-            >
-              {isSubmitting ? (
-                <Trans component={null}>Submitting</Trans>
-              ) : values.id ? (
-                <Trans component={null}>Update collection</Trans>
-              ) : (
-                <Trans component={null}>Create collection</Trans>
-              )}
-            </Button>
-          </Layout.Footer>
-        </Layout>
-      )}
-    </Formik>
+    <Layout testID="onboarding.collection.screen">
+      <Layout.Main>
+        <Trans>What are you learning?</Trans>
+        <TextField
+          accessibilityLabel={i18n.t(msg`Enter a collection name`)}
+          autoFocus
+          error={errors.name}
+          onChangeText={(value) => handleChange('name')(value)}
+          onSubmitEditing={() => handleSubmit()}
+          placeholder={i18n.t(msg`Collection name`)}
+          testID="onboarding.collection.name"
+          value={values.name}
+        />
+      </Layout.Main>
+      <Layout.Footer>
+        <Button
+          testID="onboarding.collection.cta"
+          onPress={() => handleSubmit()}
+        >
+          {isSubmitting ? (
+            <Trans component={null}>Submitting</Trans>
+          ) : values.id ? (
+            <Trans component={null}>Update collection</Trans>
+          ) : (
+            <Trans component={null}>Create collection</Trans>
+          )}
+        </Button>
+      </Layout.Footer>
+    </Layout>
   )
 }
---
+++ src/onboarding/ui/CollectionScreen/useForm.ts
@@ -0,0 +1,76 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { type FormikConfig, useFormik } from 'formik'
+import {
+  createCollectionMutation,
+  updateCollectionMutation,
+} from '@/collections/operations'
+import { log } from '@/telemetry'
+import { onboardingCollectionQuery } from '../../operations'
+
+type Values = { id?: number; name: string }
+
+interface Params {
+  initialValues: Values
+  onSubmitSuccess?: () => void
+  onSubmitError?: (error: Error, retry: () => void) => void
+  validationSchema: FormikConfig<Values>['validationSchema']
+}
+
+export default function useForm({
+  initialValues,
+  onSubmitSuccess,
+  onSubmitError,
+  validationSchema,
+}: Params) {
+  const { mutateAsync: createCollection } = useMutation(
+    createCollectionMutation,
+  )
+  const { mutateAsync: updateCollection } = useMutation(
+    updateCollectionMutation,
+  )
+  const queryClient = useQueryClient()
+
+  const onSubmit: FormikConfig<typeof initialValues>['onSubmit'] = async (
+    values,
+    { setFieldValue },
+  ) => {
+    type Action = typeof values.id extends number
+      ? typeof updateCollection
+      : typeof createCollection
+
+    const doesCollectionExist = values.id !== undefined
+
+    const submit = (
+      doesCollectionExist ? updateCollection : createCollection
+    ) as Action
+
+    const handlers: Parameters<Action>[1] = {
+      async onSuccess({ id }) {
+        await queryClient.invalidateQueries({
+          queryKey: onboardingCollectionQuery.queryKey,
+        })
+
+        await setFieldValue('id', id)
+
+        onSubmitSuccess?.()
+      },
+      onError(error) {
+        log.error(error)
+        onSubmitError?.(error, async () => submit(values, handlers))
+      },
+    }
+
+    try {
+      await submit(values, handlers)
+    } catch {
+      // Errors handled by `submit`'s error handler
+    }
+  }
+
+  return useFormik({
+    enableReinitialize: true,
+    initialValues,
+    onSubmit,
+    validationSchema,
+  })
+}
