92100506d387d20e002f7d49f1c49fa053f6f961
Wing-Hou Chan <wing@wii.ng>
2025-03-04 15:21:48 +0000

    feat(ui): style switch

--- src/ui/Switch/index.tsx
+++ src/ui/Switch/index.tsx
@@ -1,40 +1,61 @@
-import {
-  Pressable,
-  Switch as NativeSwitch,
-  SwitchProps,
-  Text,
-} from 'react-native'
+import * as Haptics from 'expo-haptics'
+import { useState } from 'react'
+import { Pressable, SwitchProps, Text, View } from 'react-native'
 import styles from './styles'
 
-interface Props extends SwitchProps {
+interface Props
+  extends Pick<
+    SwitchProps,
+    'disabled' | 'onChange' | 'onValueChange' | 'testID' | 'value'
+  > {
   label: string
 }
 
-export default function Switch({
-  label,
-  onValueChange,
-  testID,
-  value,
-  ...props
-}: Props) {
-  const handleLabelPress = () => {
+export default function Switch({ label, onValueChange, testID, value }: Props) {
+  /**
+   * Allows changes to variants to change the style
+   *
+   * @see {@link https://github.com/jpudysz/react-native-unistyles/issues/368}
+   */
+  // eslint-disable-next-line react-compiler/react-compiler
+  'use no memo'
+
+  const [pressed, setPressed] = useState(false)
+
+  const handlePress = () => {
     onValueChange?.(!value)
+
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Soft)
   }
+
+  const handleKnobPressIn = () => {
+    setPressed(true)
+  }
+
+  const handleKnobPressOut = () => {
+    setPressed(false)
+  }
+
+  styles.useVariants({ pressed, value })
 
   return (
     <Pressable
       accessibilityRole="switch"
-      onPress={handleLabelPress}
+      onPress={handlePress}
       style={styles.pressable}
       testID={testID && `${testID}.switch`}
     >
       <Text testID={testID && `${testID}.switch.label`}>{label}</Text>
-      <NativeSwitch
-        onValueChange={onValueChange}
-        testID={testID && `${testID}.switch.input`}
-        value={value}
-        {...props}
-      />
+      <Pressable
+        accessibilityRole="switch"
+        style={styles.track}
+        onPress={handlePress}
+        onPressIn={handleKnobPressIn}
+        onPressOut={handleKnobPressOut}
+        testID={testID && `${testID}.switch.track`}
+      >
+        <View style={styles.knob} testID={testID && `${testID}.switch.knob`} />
+      </Pressable>
     </Pressable>
   )
 }
--- src/ui/Switch/styles.ts
+++ src/ui/Switch/styles.ts
@@ -1,12 +1,59 @@
 import { StyleSheet } from 'react-native-unistyles'
 
-const styles = StyleSheet.create(({ spacing }) => ({
-  pressable: {
-    alignItems: 'center',
-    flexDirection: 'row',
-    gap: spacing[2],
-    justifyContent: 'space-between',
-  },
-}))
+const styles = StyleSheet.create(({ colors, radii, sizes, spacing }) => {
+  const size = 28
+  const inset = 2
+
+  return {
+    pressable: {
+      alignItems: 'center',
+      flexDirection: 'row',
+      gap: spacing[2],
+      justifyContent: 'space-between',
+    },
+    track: {
+      borderRadius: radii[2],
+      height: size + inset * 2,
+      width: size * 2,
+
+      variants: {
+        value: {
+          false: {
+            backgroundColor: colors.neutral[2],
+          },
+          true: {
+            backgroundColor: colors.green[6],
+          },
+        },
+      },
+    },
+    knob: {
+      backgroundColor: colors.neutral[0],
+      borderRadius: radii[2],
+      height: size,
+      insetBlockStart: inset,
+      position: 'absolute',
+
+      variants: {
+        pressed: {
+          false: {
+            width: size,
+          },
+          true: {
+            width: size + 4,
+          },
+        },
+        value: {
+          false: {
+            insetInlineStart: inset,
+          },
+          true: {
+            insetInlineEnd: inset,
+          },
+        },
+      },
+    },
+  }
+})
 
 export default styles
