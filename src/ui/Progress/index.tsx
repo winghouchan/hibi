39ebb584c043c09e87be524885db241df2af48f7
Wing-Hou Chan <wing@wii.ng>
2025-03-04 17:06:05 +0000

    fix(ui): make progress bar accessible

--- src/ui/Progress/index.tsx
+++ src/ui/Progress/index.tsx
@@ -9,6 +9,7 @@ interface Props {
 export default function Progress({ label, value }: Props) {
   return (
     <View
+      accessible={true}
       accessibilityLabel={`${label ? `${label} ` : ''}${value}%`}
       accessibilityRole="progressbar"
       style={style.track}
