c299ea71ca025ae48ea0175d27821bb23caa7e0d
Wing-Hou Chan <wing@wii.ng>
2025-03-07 15:33:24 +0000

    feat(note): style note config panel in note editor

--- src/notes/ui/NoteEditor/styles.tsx
+++ src/notes/ui/NoteEditor/styles.tsx
@@ -1,14 +1,21 @@
 import { StyleSheet } from 'react-native-unistyles'
 
-const styles = StyleSheet.create(({ spacing }, runtime) => ({
-  keyboardAvoidingView: {
-    flex: 1,
-    paddingBottom: runtime.insets.ime,
-  },
+const styles = StyleSheet.create(
+  ({ borderWidths, colors, radii, spacing }, runtime) => ({
+    keyboardAvoidingView: {
+      flex: 1,
+      paddingBottom: runtime.insets.ime,
+    },
 
-  settings: {
-    gap: spacing[2],
-  },
-}))
+    settings: {
+      backgroundColor: colors.neutral[1].background,
+      borderColor: colors.neutral[0].border[0],
+      borderWidth: borderWidths[2],
+      borderRadius: radii[4],
+      gap: spacing[2],
+      padding: spacing[4],
+    },
+  }),
+)
 
 export default styles
