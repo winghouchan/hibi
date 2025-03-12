d712ea5380dd66bba3b096dffe7d062eed7e5a86
Wing-Hou Chan <wing@wii.ng>
2025-03-12 14:19:19 +0000

    feat(collection): style collection notes list

--- src/collections/ui/CollectionScreen/List.tsx
+++ src/collections/ui/CollectionScreen/List.tsx
@@ -1,32 +1,68 @@
 import { Link } from 'expo-router'
-import { FlatList, FlatListProps } from 'react-native'
-import { StyleSheet } from 'react-native-unistyles'
-import { note } from '@/notes/schema'
+import { FlatList, FlatListProps, View } from 'react-native'
+import { useUnistyles } from 'react-native-unistyles'
+import { note, noteField } from '@/notes/schema'
+import { Card } from '@/notes/ui'
 
-type Props = Omit<FlatListProps<typeof note.$inferSelect>, 'renderItem'>
+type Props = Omit<
+  FlatListProps<{
+    id: (typeof note.$inferSelect)['id']
+    fields: (typeof noteField.$inferSelect)[]
+  }>,
+  'renderItem'
+>
 
-const styles = StyleSheet.create(({ spacing }, { insets }) => ({
-  content: {
-    paddingBottom: spacing[4],
-    paddingLeft: insets.left + spacing[4],
-    paddingRight: insets.right + spacing[4],
-  },
-}))
+const columnsByBreakpoint = {
+  extraSmall: 2,
+  small: 4,
+  medium: 6,
+  large: 6,
+} as const
 
-export default function List({ data }: Props) {
+function isSupportedBreakpoint(
+  breakpoint: string,
+): breakpoint is keyof typeof columnsByBreakpoint {
+  return breakpoint in columnsByBreakpoint
+}
+
+export default function List(props: Props) {
+  const {
+    theme,
+    rt: { breakpoint = 'extraSmall' },
+  } = useUnistyles()
+  const columns = isSupportedBreakpoint(breakpoint)
+    ? columnsByBreakpoint[breakpoint]
+    : columnsByBreakpoint.extraSmall
+
   return (
     <FlatList
-      contentContainerStyle={styles.content}
-      data={data}
+      alwaysBounceVertical={false}
+      key={columns}
+      numColumns={columns}
+      contentContainerStyle={{
+        paddingTop: theme.spacing[4],
+        paddingBottom: theme.spacing[4],
+        paddingLeft: theme.spacing[4] - theme.spacing[3] / 2,
+        paddingRight: theme.spacing[4] - theme.spacing[3] / 2,
+      }}
       renderItem={({ item: note }) => (
-        <Link
-          key={note.id}
-          href={`/note/${note.id}`}
-          testID="library.collection.note.link"
+        <View
+          style={{
+            flex: 1,
+            maxWidth: `${(1 / columns) * 100}%`,
+            padding: theme.spacing[3] / 2,
+          }}
         >
-          {JSON.stringify(note, null, 2)}
-        </Link>
+          <Link
+            style={{ aspectRatio: 4 / 5 }}
+            key={note.id}
+            href={`/note/${note.id}`}
+          >
+            <Card fields={note.fields} />
+          </Link>
+        </View>
       )}
+      {...props}
     />
   )
 }
