17ec6c6d718d0bcca08b7a7f9d23b232337a0bf5
Wing-Hou Chan <wing@wii.ng>
2025-04-09 15:32:31 +0100

    perf(note): add benchmark for `createNote` function

---
+++ src/notes/operations/createNote/createNote.bench.ts
@@ -0,0 +1,37 @@
+import { measureAsyncFunction } from 'reassure'
+import { collection } from '@/collections/schema'
+import { mockDatabase } from 'test/utils'
+
+describe('createNote', () => {
+  test.each([
+    {
+      name: 'when creating a note',
+      fixture: {
+        collection: {
+          name: 'Collection Name',
+        },
+      },
+      input: {
+        fields: [[{ value: 'Front' }], [{ value: 'Back' }]],
+        config: {
+          reversible: true,
+          separable: true,
+        },
+      },
+    },
+  ])('$name', async ({ fixture, input }) => {
+    const { database, resetDatabaseMock } = await mockDatabase()
+    const { default: createNote } = await import('./createNote')
+
+    const [{ id: collectionId }] = await database
+      .insert(collection)
+      .values(fixture.collection)
+      .returning()
+
+    await measureAsyncFunction(
+      async () => await createNote({ collections: [collectionId], ...input }),
+    )
+
+    resetDatabaseMock()
+  })
+})
