866fa703850803e822175bacf3c4b327d2ccca01
Wing-Hou Chan <wing@wii.ng>
2025-05-08 18:00:12 +0100

    perf(review): add benchmark for `getCurrentStreak` function

---
+++ src/reviews/operations/getCurrentStreak/getCurrentStreak.bench.ts
@@ -0,0 +1,72 @@
+import { sub } from 'date-fns'
+import { random } from 'lodash'
+import { measureAsyncFunction } from 'reassure'
+import { note } from '@/notes/schema'
+import { mockDatabase } from 'test/utils'
+import { review, reviewable } from '../../schema'
+
+function mockReview() {
+  return {
+    dueFuzzed: false,
+    duration: random(1000, 10000),
+    learningEnabled: false,
+    maxInterval: 100,
+    rating: 1,
+    retention: 0.8 * 100,
+    weights: Array.from({ length: 19 }, () => 0),
+  }
+}
+
+describe('getCurrentStreak', () => {
+  test.each([
+    {
+      name: 'when 1 review has been completed',
+      variables: {
+        reviews: 1,
+      },
+    },
+    {
+      name: 'when 10 reviews have been completed',
+      variables: {
+        reviews: 10,
+      },
+    },
+    {
+      name: 'when 100 reviews have been completed',
+      variables: {
+        reviews: 100,
+      },
+    },
+    {
+      name: 'when 1000 reviews have been completed',
+      variables: {
+        reviews: 1000,
+      },
+    },
+  ])('$name', async ({ variables }) => {
+    const { database, resetDatabaseMock } = await mockDatabase()
+    const { default: getCurrentStreak } = await import('./getCurrentStreak')
+
+    const [{ id: noteId }] = await database
+      .insert(note)
+      .values({ reversible: false, separable: false })
+      .returning()
+    const [{ id: reviewableId }] = await database
+      .insert(reviewable)
+      .values({ note: noteId })
+      .returning()
+    const reviews = Array.from({ length: variables.reviews }, (_, index) => ({
+      createdAt: sub(new Date(), { days: variables.reviews - index }),
+      reviewable: reviewableId,
+      ...mockReview(),
+    }))
+
+    if (reviews.length > 0) {
+      await database.insert(review).values(reviews)
+    }
+
+    await measureAsyncFunction(() => getCurrentStreak())
+
+    resetDatabaseMock()
+  })
+})
