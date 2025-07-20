af519d4dfe1355fc02689dd9409f2a09b4e9f037
Wing-Hou Chan <wing@wii.ng>
2025-07-20 16:07:58 +0100

    build(commit): stop duplicate test runs
    `lint-staged` was configured to run tests in separate tasks. One task was run when JavaScript files
    changed and the other when TypeScript files changed. This was a problem when both JavaScript and
    TypeScript files changed as both tasks run concurrently. This made tests run very slowly and prone
    to failing due to timing out. The config has been updated to run tests in its own task when there
    are changes to either JavaScript or TypeScript files.
    
    The test command has also been updated to use the `findRelatedTests` option instead of `onlyChanged`
    as it is more precise in running only the necessary tests. `passWithNoTests` has also been specified
    to handle the case that there are no related tests.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -5,15 +5,12 @@ const typeCheck = ['tsc --project tsconfig.json --noEmit']
 const lint = (files) => ['eslint --fix'].map(joinCommandWithFiles(files))
 const prettier = (files) =>
   ['prettier --write --ignore-unknown'].map(joinCommandWithFiles(files))
-const test = ['jest --onlyChanged']
+const test = (files) =>
+  ['jest --passWithNoTests --findRelatedTests'].map(joinCommandWithFiles(files))
 
 module.exports = {
   '**/!(*.{js,jsx,ts,tsx})': (files) => [...prettier(files)],
-  '*.{js,jsx}': (files) => [...lint(files), ...prettier(files), ...test],
-  '*.{ts,tsx}': (files) => [
-    ...typeCheck,
-    ...lint(files),
-    ...prettier(files),
-    ...test,
-  ],
+  '*.{js,jsx}': (files) => [...lint(files), ...prettier(files)],
+  '*.{ts,tsx}': (files) => [...typeCheck, ...lint(files), ...prettier(files)],
+  '*.{js,jsx,ts,tsx}': (files) => [...test(files)],
 }
