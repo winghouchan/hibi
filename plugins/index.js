c32c1206f712d1662b5e226be7322ade934e14e0
Wing-Hou Chan <wing@wii.ng>
2025-08-13 02:15:31 +0100

    build(android): enable gradle build cache
    Speeds up the Gradle build phase. Saves approximately an additional 2 minutes in CI.

--- plugins/index.js
+++ plugins/index.js
@@ -1,9 +1,11 @@
 const withAppVariants = require('./withAppVariants')
 const withExpoUpdates = require('./withExpoUpdates')
+const withGradleBuildCache = require('./withGradleBuildCache')
 const withGradleConfigurationCache = require('./withGradleConfigurationCache')
 
 module.exports = {
   withAppVariants,
   withExpoUpdates,
+  withGradleBuildCache,
   withGradleConfigurationCache,
 }
