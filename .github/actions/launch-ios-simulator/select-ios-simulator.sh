8871b6e8becdd2c7c58867b37ed7ebe2cd574e8a
Wing-Hou Chan <wing@wii.ng>
2025-08-15 21:40:10 +0100

    style(sh): fix shell linter warnings

--- .github/actions/launch-ios-simulator/select-ios-simulator.sh
+++ .github/actions/launch-ios-simulator/select-ios-simulator.sh
@@ -5,6 +5,9 @@ devices=$(xcrun simctl list devices --json | jq ".devices")
 
 echo "Available devices: $devices"
 
+# Disable `SC2154 (warning)` for unassigned references to `os`, `os_version`,
+# and `model`. They are assigned from GitHub actions. See `action.yaml`.
+# shellcheck disable=SC2154 
 filter="to_entries | map(select(.key | contains(\"$os-${os_version//[.]/-}\"))) | .[0].value | map(select(.name == \"$model\")) | .[0].udid"
 
 echo "Finding device with filter: $filter"
@@ -13,4 +16,4 @@ udid=$(echo "$devices" | jq "$filter")
 
 echo "Device found with UDID: $udid"
 
-echo "udid=$udid" >> $GITHUB_OUTPUT
+echo "udid=$udid" >> "$GITHUB_OUTPUT"
