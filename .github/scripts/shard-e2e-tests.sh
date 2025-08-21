55724dd8f2b2bbea4dddb81751493f630b295408
Wing-Hou Chan <wing@wii.ng>
2025-08-21 20:51:44 +0100

    ci(test): shard end-to-end tests into groups of equal size
    Currently, the end-to-end tests are sharded by feature area. While this successfully reduces the
    run time of end-to-end tests as tests are parallelised, some feature areas have more tests which
    mean they take longer to run. Distributing tests across shards equally will reduce the run time
    even more. The number of shards has been set to 5 because that is the max concurrency of macOS
    runners ([see documentation][1]):
    
    [1]: https://docs.github.com/en/actions/reference/limits#job-concurrency-limits-for-github-hosted-runners

--- .github/scripts/shard-e2e-tests.sh
+++ .github/scripts/shard-e2e-tests.sh
@@ -4,6 +4,20 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+group=feature
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -g | --group)
+      group="$2"
+      shift 2
+      ;;
+    * )
+      break
+      ;;
+  esac
+done
+
 shard_by_feature() {
   # Assumes the working directory is the `e2e/tests` directory
   find ./* \
@@ -13,10 +27,48 @@ shard_by_feature() {
     -exec sh -c 'printf "%s\n" "${1#./}"' sh {} \;
 }
 
-convert_to_json_array() {
-  jq --compact-output --raw-input --slurp 'split("\n")[:-1]'
+shard_into_number_of_groups() {
+  local number_of_groups=$1
+
+  # Assumes the working directory is the `e2e/tests` directory
+  local test_paths
+  test_paths=$(find ./* \
+    -name '*.yaml' \
+    ! -name 'config.yaml' \
+    ! -path '*__subflows__*' \
+    -exec sh -c 'printf "%s\n" "${1#./}"' sh {} \;
+  )
+
+  local count
+  count=$(echo "$test_paths" | wc -l)
+
+  local group_size=$(((count + number_of_groups - 1) / number_of_groups))
+
+  echo "$test_paths" | xargs -n "$group_size"
 }
 
-shards=$(shard_by_feature | convert_to_json_array)
+shard() {
+  if [[ "$group" =~ ^[0-9]+$ ]] && [ "$group" -gt 0 ]; then
+    shard_into_number_of_groups "$group"
+  else
+    shard_by_feature
+  fi
+}
 
-echo "shards=$shards" >> "$GITHUB_OUTPUT"
+to_json() {
+  jq \
+    --compact-output \
+    --raw-input \
+    --slurp \
+    'split("\n")[:-1] 
+      | to_entries
+      | map({ name: ("shard-" + (.key | tostring)), tests: .value})'
+}
+
+shards=$(shard | to_json)
+
+if [ "${CI:-}" == "true" ]; then
+  echo "shards=$shards" >> "$GITHUB_OUTPUT"
+else
+  echo "$shards"
+fi
