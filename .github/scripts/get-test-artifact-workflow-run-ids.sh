4d14f8914b781d236569863e0dfc56e5823c06d9
Wing-Hou Chan <wing@wii.ng>
2025-08-26 13:47:28 +0100

    ci(plan): skip android build job if past build can be re-used
    The Android build job (with a populated build, compiler, and configuration cache) currently takes
    approximately 8-10 minutes. This could be skipped if there are no native changes and a past build
    is available for end-to-end tests.

---
+++ .github/scripts/get-test-artifact-workflow-run-ids.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+artifacts_endpoint="/repos/{owner}/{repo}/actions/artifacts"
+
+# Filter for the most relevant artifact, prioritising for artifacts created from
+# the same branch, otherwise falling back to the main branch.
+filter=".artifacts
+  | map(
+      select(
+        .expired == false and (
+          .workflow_run.head_branch == \"${GITHUB_HEAD_REF:-}\" or
+          .workflow_run.head_branch == \"${GITHUB_REF_NAME:-}\" or
+          .workflow_run.head_branch == \"main\"
+        )
+      )
+    )
+  | sort_by(
+      (
+        .worfklow_run.head_branch |
+        if . == \"${GITHUB_HEAD_REF:-}\" then 0
+        elif . == \"${GITHUB_REF_NAME:-}\" then 1
+        else 3 end
+      )
+    )
+  | .[0].workflow_run.id"
+
+android_artifact_workflow_run_id=$(gh api \
+  "${artifacts_endpoint}" \
+  --method GET \
+  --header 'Accept: application/vnd.github+json' \
+  --field "name=build-android" \
+  --jq "${filter}")
+
+ids="{ \"android\": ${android_artifact_workflow_run_id} }"
+
+echo "ids=${ids}" >> "${GITHUB_OUTPUT}"
